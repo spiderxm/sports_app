247f40566910a874d5bcbc98c28e0c477fee909e
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-09-25 23:39:42 +0530

    fix settings

--- home_app/urls.py
+++ home_app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from home_app.views import home, sport, news, olympics, add_trial, trial_detail, apply_to_trial, trial, users_list
+from django.conf import settings
 
 app_name = 'home'
 urlpatterns = [
@@ -11,5 +13,7 @@ urlpatterns = [
     url(r'^trial/(?P<_id>\d+)/$', trial_detail, name="trial"),
     url(r'^trials/$', trial, name="trials"),
     url(r'^users/$', users_list, name="users"),
-    url(r'^apply_trial/(?P<_id>\d+)/$', apply_to_trial, name="apply_to_trial")
+    url(r'^apply_trial/(?P<_id>\d+)/$', apply_to_trial, name="apply_to_trial"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
